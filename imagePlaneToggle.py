d1a6082cc5d011c9ebc80eac3d5b7161c6acd3a3
razorbjc <47091171+razorbjc@users.noreply.github.com>
2019-05-22 02:13:39 -0400

    Update and rename imageplanetoggle.py to imagePlaneToggle.py

--- imagePlaneToggle.py
+++ imagePlaneToggle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2.7
 
 """
+For Maya 2016
 Toggles imageplane visibility of current camera
 May 2019
 __author__: James Chan
@@ -9,7 +10,7 @@ __author__: James Chan
 import maya.cmds as cmds
 
 
-def imageplanetoggle():
+def imagePlaneToggle():
     current_cam = cmds.modelPanel(cmds.getPanel(wf=True), q=True, cam=True)
     current_cam_shape = cmds.listRelatives(current_cam, c=True, f=True)
     imgplanetrans = cmds.listConnections(current_cam_shape)
